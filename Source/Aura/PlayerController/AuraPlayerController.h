203854d21bd7a6f503e34d58d012e5efe7e1bd46
MajorTomAW <tom.wrnr.private@gmail.com>
2023-11-20 20:54:04 +0100

    input subsystem

---
+++ Source/Aura/PlayerController/AuraPlayerController.h
@@ -0,0 +1,16 @@
+// Copyright © 2023 MajorT. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
