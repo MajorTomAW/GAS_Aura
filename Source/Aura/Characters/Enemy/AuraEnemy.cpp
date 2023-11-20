c9ca6dfe93d35a52745956f6ec5190daab0dd4d0
MajorTomAW <tom.wrnr.private@gmail.com>
2023-11-20 22:27:50 +0100

    highlight material

--- Source/Aura/Characters/Enemy/AuraEnemy.cpp
+++ Source/Aura/Characters/Enemy/AuraEnemy.cpp
@@ -2,13 +2,20 @@
 
 
 #include "AuraEnemy.h"
+#include "Aura/Aura.h"
 
 void AAuraEnemy::HighlightActor()
 {
 	bHighlighted = true;
+	GetMesh()->SetRenderCustomDepth(true);
+	WeaponMesh->SetRenderCustomDepth(true);
+	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	WeaponMesh->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
 	bHighlighted = false;
+	GetMesh()->SetRenderCustomDepth(false);
+	WeaponMesh->SetRenderCustomDepth(false);
 }
